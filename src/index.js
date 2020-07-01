09e8659e153fee8bf811fdcabfa347f390ff3823
Nik68rus <nik200x@mail.ru>
2020-07-01 22:56:06 +0300

    добавляет store

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
-import questions from "./mocks/questions";
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {reducer} from './reducer.js';
 
-const Settings = {
-  GAME_TIME: 5,
-  ERRORS_COUNT: 3
-};
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 ReactDOM.render(
-    <App
-      gameTime={Settings.GAME_TIME}
-      errorCount={Settings.ERRORS_COUNT}
-      questions={questions}
-    />,
+    <Provider store={store}>
+      <App />,
+    </Provider>,
     document.querySelector(`#root`)
 );
 
